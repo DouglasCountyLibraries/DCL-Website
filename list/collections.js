6c3093e0453f1ede09ef1d3891abdd18ca56b3a8
mmorrow <mmorrow@dclibraries.org>
2017-08-08 09:02:45 -0600

    add ;

--- list/collections.js
+++ list/collections.js
@@ -1,6 +1,6 @@
 
 //var baseURI = "http:///dev.dclcollections.dcl.lan/";
-var baseURI = "https://dclcollections.douglascountylibraries.org
+var baseURI = "https://dclcollections.douglascountylibraries.org";
 var baseDCLOrgURI = "https://dcl.org/list/";
 // <div id="covers-${title}" class ="container shelf-container text-center
 var a = window.location.toString(); 
