b68679dd4da6a33baf2c29b4ec20210ae7e13ca6
whataboutdale <djarrett@dclibraries.org>
2018-02-06 16:39:48 -0700

    new smooth scoll

--- shared-assets/js/dcl.js
+++ shared-assets/js/dcl.js
@@ -21,21 +21,30 @@ jQuery.get("https://s3.amazonaws.com/website.alert/alert.txt")
 // smooth page scroll for page anchors
 /* ------------------------------------------------------------ */
 
-	$(function() {
-$('a[href*=#]:not([href=#]):not(.carousel-control):not(.tab)').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top - 50
-	        }, 800);
-	        return false;
-	      }
-	    }
-	  });
-	});
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
 
 // --------------------------------------------------------------------------------------------------------
 // jquery-match-height master by @liabru
