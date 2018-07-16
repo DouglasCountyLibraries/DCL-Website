c224fcca58823772e69a4afa7ca27b8fcbf34ef5
whataboutdale <djarrett@dclibraries.org>
2018-07-16 14:10:50 -0600

    fade in change

--- staff-picks/biblioList.js
+++ staff-picks/biblioList.js
@@ -12,7 +12,7 @@ function createAllPageTitle(title)
 {
   var x = [
      '<div class="text-center">',
-      '<h1 class="jumbo-text text-uppercase font-weight-400">What' + "'s"  + 'New</h1>',
+      '<h1 class="jumbo-text text-uppercase font-weight-400 fade-in-text">What' + "'s"  + 'New</h1>',
       '<div class="separator-container padding-bottom-15">',
        ' <div class="separator line-separator"><i class="fa fa-star-o"></i></div>',
      ' </div>',
@@ -54,7 +54,7 @@ function createPostShelves(title) {
 // <div class ="row padding-bottom-15 border-top">
 //            <div class ="container">
 //                <div class ="text-center">
-//                    <h1 class ="jumbo-text text-uppercase font-weight-400">${title}</h1>
+//                    <h1 class ="jumbo-text text-uppercase font-weight-400 fade-in-text">${title}</h1>
 
 //                </div>
 //                <div class ="row">
