a32f2f277616c4a25fddd767e5861f2734801b83
Dale Jarrett <whataboutdale@users.noreply.github.com>
2024-04-24 09:15:55 -0600

    using jquery

--- shared-assets/utilities/communico/dcl-defined.js
+++ shared-assets/utilities/communico/dcl-defined.js
@@ -7,17 +7,17 @@
   
 // add a link to library card application on login form
 /* ------------------------------------------------------------ */
-document.addEventListener('DOMContentLoaded', function () {
-  function addCardLink() {
-    var link = document.createElement('a');
-    link.href = 'https://www.dcl.org/library-card/';
-    link.textContent = 'Get a library card.';
-    link.target = '_blank';
-    link.classList.add('library-card-link');
-    var targetElement = document.querySelector('.amnp-login-form > .am-field-password > .am-form-desc');
-    if (targetElement) {
-      targetElement.appendChild(link);
+$(document).ready(function() {
+    function addCardLink() {
+        var link = document.createElement('a');
+        link.href = 'https://www.dcl.org/library-card/';
+        link.textContent = 'Get a library card.';
+        link.target = '_blank';
+        link.classList.add('library-card-link');
+        var targetElement = document.querySelector('.amnp-login-form > .am-field-password > .am-form-desc');
+        if (targetElement) {
+            targetElement.appendChild(link);
+        }
     }
-  }
-  addCardLink();
+    addCardLink();
 });
