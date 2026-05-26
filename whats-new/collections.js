a1810528a216d952962c6e3e965aa7a44f373c1c
Dale Jarrett <djarrett@dclibraries.org>
2026-05-26 09:09:34 -0600

    updated headings

--- whats-new/collections.js
+++ whats-new/collections.js
@@ -25,9 +25,9 @@ function createAllPageTitle(title)
 {
   var x = [
      '<div class="text-center">',
-      '<h1 class="jumbo-text text-uppercase margin-bottom-5 font-weight-400">What&#39;s New</h1>',
+      '<h1 class="page-heading">What&#39;s New</h1>',
    ' </div>',
-     '<h2 class="no-margin-top margin-bottom-5 text-center">' + title + '</h2>',
+     '<h2 class="margin-bottom-5">' + title + '</h2>',
       '<div><a class="text-uppercase font-weight-400" href="../whats-new">« Return to Categories</a></div>'     
 	 ].join('\n');
       
